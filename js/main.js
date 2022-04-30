66759cde5b4c8ef2395dd2136fd80f93ad2dade2
Jared Joshua Balajadia <46180962+Zukashikute@users.noreply.github.com>
2022-05-01 02:16:40 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,41 @@
+const links = [
+   {
+      label: "Week 1",
+      url: "week1/index.html"
+   },
+
+   {
+      label: "Week 2",
+      url: "week2/index.html"
+   },
+
+   {
+      label: "Week 3",
+      url: "week3/index.html"
+   }
+]
+
+// Target Main Element
+let linkContainer = document.getElementById("homeworkLinks");
+
+// Created Elements for HTML
+let newOlist = document.createElement("ol");
+
+linkContainer.appendChild(newOlist);
+
+function loadLinks() {
+   for (link in links) {
+      // Create elements according to how many objects in links[].
+      let newBulletList = document.createElement("li");
+      let newLink = document.createElement("a");
+      let textLabel = document.createTextNode(links[link].label);
+      
+      newOlist.appendChild(newBulletList);
+      newBulletList.appendChild(newLink);
+      newLink.appendChild(textLabel);
+      newLink.href = (links[link].url);
+   }
+
+}
+
+loadLinks();
