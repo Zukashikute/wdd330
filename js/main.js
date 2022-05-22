bb7740eadf75a00aff3a832c6f5fe0f615873898
Jared Joshua Balajadia <46180962+Zukashikute@users.noreply.github.com>
2022-05-22 13:46:21 +0800

    Week 5 Update

--- js/main.js
+++ js/main.js
@@ -12,6 +12,16 @@ const links = [
    {
       label: "Week 3",
       url: "week3/index.html"
+   },
+
+   {
+      label: "Week 4",
+      url: "week4/index.html"
+   },
+
+   {
+      label: "Week 5",
+      url: "week5/index.html"
    }
 ]
 
